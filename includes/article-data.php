ce706a2cdab713325538fdea48c4ea78bc8af725
yuriybratchenko <bratchenkoyuriy@gmail.com>
2020-10-20 21:49:11 +0300

    ADD: comments template for article data

--- includes/article-data.php
+++ includes/article-data.php
@@ -156,6 +156,16 @@ if ( ! class_exists( 'Troubleshooting_Manager_Article_Data' ) ) {
 								remove_filter( 'embed_oembed_html', array( $this, 'add_oembed_wrapper' ) );
 							?></div>
 						</div>
+                       <?php if ( comments_open() || get_comments_number() ) { comments_template(); } ?>
+                        <div class="ts-manager-help-box">
+                            <div class="ts-manager-help-box__title">Still need help?</div>
+                            <a href="https://crocoblock.com/help-center/" class="ts-manager-help-box__btn" role="button" target="_blank">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM13 19H11V17H13V19ZM15.07 11.25L14.17 12.17C13.45 12.9 13 13.5 13 15H11V14.5C11 13.4 11.45 12.4 12.17 11.67L13.41 10.41C13.78 10.05 14 9.55 14 9C14 7.9 13.1 7 12 7C10.9 7 10 7.9 10 9H8C8 6.79 9.79 5 12 5C14.21 5 16 6.79 16 9C16 9.88 15.64 10.68 15.07 11.25Z" fill="#617BE5"/>
+                                </svg>
+                                <span>Submit a request</span>
+                            </a>
+                        </div>
 					</article><?php
 					endwhile;
 					if ( $is_active_sidebar ) : ?>
